01110f76b4f3db4f3e4c9a36dd5031892b37026b
yousefeslam <117038006+Yousefeslam214@users.noreply.github.com>
2025-04-10 20:21:23 +0200

    Time: 6 ms (17.96%) | Memory: 56.6 MB (42.49%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.ts
@@ -0,0 +1,11 @@
+
+function longestCommonPrefix(strs: string[]): string {
+  for (let i = 0; i < strs[0].length; i++) {
+    for (let j = 0; j < strs.length; j++) {
+      if (strs[j][i] !== strs[0][i]) {
+        return strs[0].slice(0, i);
+      }
+    }
+  }
+  return strs[0];
+}
